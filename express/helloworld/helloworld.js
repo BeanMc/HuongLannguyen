fef38b39611f23670bcb186ee3ed372e5948c0f1
HuonglanNguyen <nguyenhuyconguneti@gmail.com>
2016-10-26 08:42:20 +0700

    express/

--- express/helloworld/helloworld.js
+++ express/helloworld/helloworld.js
@@ -1,4 +1,7 @@
 var express =require('express');
+var serveIndex = require('serve-index');
+var path= require('path');
+
 var app=express();
 var port=3000;
 
@@ -9,11 +12,17 @@ app.get('/name/:username',function (req, res) {
     res.send("<html><body>hello "+req.params['username']+"</body></html>")
     res.end('Hello Wolrd');
 })
+app.use(function (req, res, next) {
+    console.log('Time:', Date.now());
+    next();
+});
 
 app.listen(port,function () {
     console.log(app.get('env'));
     console.log("this server is running ");
 })
+
+
 exports.index = function(req, res){
     res.render('index.ejs', { title: 'Express' });
 };
